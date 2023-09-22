77ce6da6f4c7b3660d4065e8ce0fd14ae5fa7b08
Gaetan Lepage <gaetan@glepage.com>
2023-09-22 23:14:22 +0200

    add agenix

--- flake.nix
+++ flake.nix
@@ -1,17 +1,28 @@
 {
   description = "AC desktop NixOS configuration";
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    agenix = {
+      url = "github:ryantm/agenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
   outputs = {
     self,
     nixpkgs,
+    agenix,
   }: let
     hostname = "acl-desktop";
   in {
     nixosConfigurations.${hostname} = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      modules = [./nixos];
+      modules = [
+        ./nixos
+        agenix.nixosModules.default
+      ];
     };
 
     apps = let
