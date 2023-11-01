3de1ab87f02fc468e3ed0ce9efbe2e52076c4478
Gaetan Lepage <gaetan@glepage.com>
2023-11-01 18:19:07 +0100

    nfs-mount: formatting

--- nixos/server-tank-nfs.nix
+++ nixos/server-tank-nfs.nix
@@ -1,18 +1,16 @@
 {
-  fileSystems = let
-    fsType = "nfs";
-    options = [
-      "noauto"
-      "x-systemd.automount"
-      "nfsvers=4"
-
-      # disconnects after 10 minutes (i.e. 600 seconds)
-      "x-systemd.idle-timeout=600"
-    ];
-  in {
+  fileSystems = {
     "/mnt/server" = {
       device = "10.10.10.1:/tank";
-      inherit options fsType;
+      fsType = "nfs";
+      options = [
+        "noauto"
+        "x-systemd.automount"
+        "nfsvers=4"
+
+        # disconnects after 10 minutes (i.e. 600 seconds)
+        "x-systemd.idle-timeout=600"
+      ];
     };
   };
 }
