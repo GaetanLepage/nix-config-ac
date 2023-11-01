0175f1015ba2b709f0c5ba91477709bf6d0a2502
Gaetan Lepage <gaetan@glepage.com>
2023-11-01 18:19:21 +0100

    printer: stateless printer declaration

--- nixos/printer.nix
+++ nixos/printer.nix
@@ -1,10 +1,32 @@
 {pkgs, ...}: {
   services = {
-    printing.enable = true;
+    printing = {
+      enable = true;
+
+      # All state directories relating to CUPS will be removed on startup of the service.
+      stateless = true;
+    };
     avahi = {
       enable = true;
       nssmdns = true;
     };
+  };
+
+  hardware.printers = let
+    printerName = "Brother_ACL";
+  in {
+    ensureDefaultPrinter = printerName;
+    ensurePrinters = [
+      {
+        name = printerName;
+        location = "Bureau ACL";
+        deviceUri = "ipp://192.168.1.20:631/ipp/print";
+        model = "everywhere";
+        ppdOptions = {
+          PageSize = "A4";
+        };
+      }
+    ];
   };
 
   environment.systemPackages = with pkgs; [
