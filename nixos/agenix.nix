30e6ce31369cced95e9f3fd9873a431f0de10972
Gaetan Lepage <gaetan@glepage.com>
2024-03-09 22:59:36 +0100

    agenix: move master key

--- nixos/agenix.nix
+++ nixos/agenix.nix
@@ -2,7 +2,7 @@
   age.rekey = {
     localStorageDir = ../secrets;
     storageMode = "local";
-    masterIdentities = [../secrets/identity.age];
+    masterIdentities = [../identity.age];
     hostPubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEhJsg/+EB8KwUWluo036o6gnejqtubYMlCvp6D9uol+";
   };
 }
