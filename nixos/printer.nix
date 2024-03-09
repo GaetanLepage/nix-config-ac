242838c7d7f4ed6b42da01c53aeef71f3fa2a7ff
Gaetan Lepage <gaetan@glepage.com>
2024-03-09 23:08:18 +0100

    printers: disable

--- nixos/printer.nix
+++ nixos/printer.nix
@@ -12,22 +12,22 @@
     };
   };
 
-  hardware.printers = let
-    printerName = "Brother_ACL";
-  in {
-    ensureDefaultPrinter = printerName;
-    ensurePrinters = [
-      {
-        name = printerName;
-        location = "Bureau ACL";
-        deviceUri = "ipp://192.168.1.20:631/ipp/print";
-        model = "everywhere";
-        ppdOptions = {
-          PageSize = "A4";
-        };
-      }
-    ];
-  };
+  # hardware.printers = let
+  #   printerName = "Brother_ACL";
+  # in {
+  #   ensureDefaultPrinter = printerName;
+  #   ensurePrinters = [
+  #     {
+  #       name = printerName;
+  #       location = "Bureau ACL";
+  #       deviceUri = "ipp://192.168.1.20:631/ipp/print";
+  #       model = "everywhere";
+  #       ppdOptions = {
+  #         PageSize = "A4";
+  #       };
+  #     }
+  #   ];
+  # };
 
   environment.systemPackages = with pkgs; [
     gnome.simple-scan
