bb217a3fc7477ae7ccca656f125b3a602615c768
Gaetan Lepage <gaetan@glepage.com>
2024-03-10 15:00:33 +0100

    networking: fix wake on LAN persistence issue

--- nixos/networking.nix
+++ nixos/networking.nix
@@ -1,4 +1,8 @@
 {
+  pkgs,
+  lib,
+  ...
+}: {
   networking = {
     hostName = "acl-desktop";
 
@@ -7,5 +11,17 @@
     networkmanager.enable = true;
 
     firewall.enable = true;
+  };
+
+  systemd.services.wakeonlan = {
+    description = "Reenable wake on lan every boot";
+    after = ["network.target"];
+    serviceConfig = {
+      Type = "simple";
+      # RemainAfterExit = "true";
+      RemainAfterExit = true;
+      ExecStart = "${lib.getExe pkgs.ethtool} -s enp1s0 wol g";
+    };
+    wantedBy = ["default.target"];
   };
 }
