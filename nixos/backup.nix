28f56ffc49a05fff844ba02f303ec1a2f10e2a96
Gaetan Lepage <gaetan@glepage.com>
2024-03-10 12:12:02 +0100

    backup: refactoring

--- nixos/backup.nix
+++ nixos/backup.nix
@@ -1,4 +1,8 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: {
   systemd = let
     serviceName = "backup-home-to-server";
   in {
@@ -8,32 +12,38 @@
       serviceConfig = {
         Type = "oneshot";
         User = "anne-catherine";
+        ExecStart = lib.getExe (pkgs.writeShellApplication {
+          name = "backup";
+          runtimeInputs = with pkgs; [
+            rsync
+          ];
+          text = ''
+            # Do not crash the service if the rsync command fails
+            set +e
+
+            # rsync backup command
+            rsync \
+              -zravut \
+              --human-readable \
+              --delete \
+              --delete-excluded \
+              --exclude ".cache/" \
+              /home \
+              /mnt/server/lepage_family/backup/ac_desktop/
+
+            # Collect rsync exit code
+            status=$?
+
+            case $status in
+                # Catch exit code 24 as this error is not important
+                0|24) exit 0 ;;
+
+                # In case of an error other than 24, crash the service
+                *) exit 1 ;;
+            esac
+          '';
+        });
       };
-      script = ''
-        # Do not crash the service if the rsync command fails
-        set +e
-
-        # rsync backup command
-        ${pkgs.rsync}/bin/rsync \
-          -zravut \
-          --human-readable \
-          --delete \
-          --delete-excluded \
-          --exclude ".cache/" \
-          /home \
-          /mnt/server/lepage_family/backup/ac_desktop/
-
-        # Collect rsync exit code
-        status=$?
-
-        case $status in
-            # Catch exit code 24 as this error is not important
-            0|24) exit 0 ;;
-
-            # In case of an error other than 24, crash the service
-            *) exit 1 ;;
-        esac
-      '';
     };
 
     timers.${serviceName} = {
